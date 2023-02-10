b5e5fe4e85359dde512c7ed0cb8093d7c3011b40
johannahrodgers <johannahrodgers@yahoo.com>
2023-02-10 16:52:46 -0500

    Add files via upload

---
+++ docs/assets/DNA3xycorporation.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20200121
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('DNA3xycorporation_files/DNA3xycorporationMoz.css')
+fixAllIEPNGs('Media/transparent.gif');applyEffects()}
